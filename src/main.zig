f8ed325adef293c6cbcaaf3e463a862a45bcd4f8
Paul Kim <muhyun.paul.kim@gmail.com>
2025-03-12 20:22:22 +0000

    collect rest apis that will be used.

--- src/main.zig
+++ src/main.zig
@@ -2,6 +2,23 @@
 //! you are building an executable. If you are making a library, the convention
 //! is to delete this file and start with root.zig instead.
 
+// Get JQL Search:              GET  /rest/api/3/search/jql
+// Get Project:                 GET  /rest/api/3/project/{idOrKey}
+// Get Version:                 GET  /rest/api/3/version/{id}
+// Get Issue:                   GET  /rest/api/3/issue/{idOrKey}
+// Put Assign Issue:            PUT  /rest/api/3/issue/{idOrKey}/assignee
+// Get Issue Comments:          GET  /rest/api/3/issue/{idOrKey}/comment
+// Get Issue Comment:           GET  /rest/api/3/issue/{idOrKey}/comment/{id}
+// Post Add Issue Comment:      POST /rest/api/3/issue/{idOrKey}/comment
+// Put Update Issue Comment:    PUT  /rest/api/3/issue/{idOrKey}/comment/{id}
+// Del Delete Issue Comment:    DEL  /rest/api/3/issue/{idOrKey}/comment/{id}
+// Get Statuses:                GET  /rest/api/3/statuses
+// Put Update Statuses:         PUT  /rest/api/3/statuses
+// Get Status:                  GET  /rest/api/3/status/{idOrName}
+// Get Priority (?):            GET  /rest/api/3/priority/{id}
+//
+//
+
 pub fn main() !void {
     // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
     std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
@@ -40,6 +57,7 @@ test "fuzz example" {
     try std.testing.fuzz(Context{}, Context.testOne, .{});
 }
 
+const clap = @import("clap");
 const std = @import("std");
 
 /// This imports the separate module containing `root.zig`. Take a look in `build.zig` for details.

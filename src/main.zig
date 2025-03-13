f5c9f33fa1ea4d6d4ad4e8e30680c953b34cdd44
Paul Kim <muhyun.paul.kim@gmail.com>
2025-03-13 17:45:55 +0000

    add more details for jql usage.

--- src/main.zig
+++ src/main.zig
@@ -2,23 +2,97 @@
 //! you are building an executable. If you are making a library, the convention
 //! is to delete this file and start with root.zig instead.
 
-// Get JQL Search:              GET  /rest/api/3/search/jql
-// Get Project:                 GET  /rest/api/3/project/{idOrKey}
-// Get Version:                 GET  /rest/api/3/version/{id}
-// Get Issue:                   GET  /rest/api/3/issue/{idOrKey}
-// Put Assign Issue:            PUT  /rest/api/3/issue/{idOrKey}/assignee
-// Get Issue Comments:          GET  /rest/api/3/issue/{idOrKey}/comment
-// Get Issue Comment:           GET  /rest/api/3/issue/{idOrKey}/comment/{id}
-// Post Add Issue Comment:      POST /rest/api/3/issue/{idOrKey}/comment
-// Put Update Issue Comment:    PUT  /rest/api/3/issue/{idOrKey}/comment/{id}
-// Del Delete Issue Comment:    DEL  /rest/api/3/issue/{idOrKey}/comment/{id}
-// Get Statuses:                GET  /rest/api/3/statuses
-// Put Update Statuses:         PUT  /rest/api/3/statuses
-// Get Status:                  GET  /rest/api/3/status/{idOrName}
-// Get Priority (?):            GET  /rest/api/3/priority/{id}
-//
-//
-
+/// Jira REST APIs:
+/// https://developer.atlassian.com/cloud/jira/platform/rest/v3/intro/#version
+/// Get JQL Search:              GET  /rest/api/3/search/jql
+/// https://support.atlassian.com/jira-service-management-cloud/docs/use-advanced-search-with-jira-query-language-jql/
+/// useful fields:
+/// - reporter = John Smith
+/// - assignee = John Smith
+/// - project = Name
+/// - issuetype = Type
+/// - status = Status
+/// - comment
+/// - created
+/// - description
+/// - fixVersion
+/// - type
+/// - reporter
+/// - priority
+/// - labels
+/// - due
+/// - parent
+/// - updated
+/// "keywords":
+/// - AND
+/// - OR
+/// - NOT
+/// - EMPTY
+/// - NULL - same as empty
+/// - ORDER BY
+/// ops:
+/// - '='
+/// - '!='
+/// - '>'
+/// - '>='
+/// - '<'
+/// - '<='
+/// - in
+/// - not in
+/// - '~' (contains)
+/// - '!~' (does not contain)
+/// - is
+/// - is not
+/// - was
+/// - was in
+/// - was not in
+/// - was not
+/// - changed
+/// functions:
+/// - approved()SLA?
+/// - approver()SLA?
+/// - completed()SLA?
+/// - currentLogin()
+/// - currentUser()
+/// - endOfDay()
+/// - endOfWeek()
+/// - endOfMonth()
+/// - endOfYear()
+/// - linkedIssue
+/// - linkedIssues()
+/// - myApproval()
+/// - myPendingApproval()
+/// - myPending()
+/// - now()
+/// - openSprints()
+/// - paused()SLA?
+/// - pending()SLA?
+/// - pendingBy()SLA?
+/// - releasedVersions()
+/// - remaining()SLA?
+/// - running()SLA?
+/// - startOfDay()
+/// - startOfWeek()
+/// - startOfMonth()
+/// - startOfYear()
+/// - subtaskIssueTypes()
+/// - unreleasedVersions()
+/// - updatedBy()
+///
+/// Get Project:                 GET  /rest/api/3/project/{idOrKey}
+/// Get Version:                 GET  /rest/api/3/version/{id}
+/// Get Issue:                   GET  /rest/api/3/issue/{idOrKey}
+/// Put Assign Issue:            PUT  /rest/api/3/issue/{idOrKey}/assignee
+/// Get Issue Comments:          GET  /rest/api/3/issue/{idOrKey}/comment
+/// Get Issue Comment:           GET  /rest/api/3/issue/{idOrKey}/comment/{id}
+/// Post Add Issue Comment:      POST /rest/api/3/issue/{idOrKey}/comment
+/// Put Update Issue Comment:    PUT  /rest/api/3/issue/{idOrKey}/comment/{id}
+/// Del Delete Issue Comment:    DEL  /rest/api/3/issue/{idOrKey}/comment/{id}
+/// Get Statuses:                GET  /rest/api/3/statuses
+/// Put Update Statuses:         PUT  /rest/api/3/statuses
+/// Get Status:                  GET  /rest/api/3/status/{idOrName}
+/// Get Priority (?):            GET  /rest/api/3/priority/{id}
+///
 pub fn main() !void {
     // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
     std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
@@ -57,8 +131,9 @@ test "fuzz example" {
     try std.testing.fuzz(Context{}, Context.testOne, .{});
 }
 
-const clap = @import("clap");
 const std = @import("std");
+const clap = @import("clap");
+const vaxis = @import("vaxis");
 
 /// This imports the separate module containing `root.zig`. Take a look in `build.zig` for details.
 const lib = @import("hercules_lib");
